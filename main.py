9fdf37a4fb550f118e5eef89abf9f1d9d330ea4f
colinknox <colinwknox@gmail.com>
2026-03-01 11:03:12 -0500

    created NotificationService class

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+class NotificationService:
+    def __init__(self, recipient):
+        self.recipient = recipient
+
+    def format_message(self, title, content):
+        raise NotImplementedError("Must implement message formatting")
+
+    def send_message(self, formatted_message):
+        raise NotImplementedError("Must implement message sending")
+
+    def notify(self, title, content):
+        formatted_message = self.format_message(title, content)
+        return self.send_message(formatted_message)
+
+
+
 def main():
-    print("Hello from message-notification-system!")
+    test = NotificationService("Adam")
+    
+    # print(f"DEBUG: Format message = {test.format_message("Flipping Awesome News!", "i tootin' like putin")}")
+    # print(f"DEBUG: Send message = {test.send_message("wowie zowie")}")
 
 
 if __name__ == "__main__":
